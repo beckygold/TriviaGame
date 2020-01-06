0d0fbbd533e700480357ee820b7eab32bc74f0cb
Becky Gold <43392694+rebeccagoldstein@users.noreply.github.com>
2020-01-06 12:53:34 -0700

    Added array of questions

---
+++ assets/javascript/questions.js
@@ -0,0 +1,123 @@
+// Array of trivia questions and answers from Open Trivia Database 
+var questions = [
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Which company did Valve cooperate with in the creation of the Vive?",
+        "correct_answer": "HTC",
+        "incorrect_answers": [
+            "Oculus",
+            "Google",
+            "Razer"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "On a dartboard, what number is directly opposite No. 1?",
+        "correct_answer": "19",
+        "incorrect_answers": [
+            "20",
+            "12",
+            "15"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "What alcoholic drink is made from molasses?",
+        "correct_answer": "Rum",
+        "incorrect_answers": [
+            "Gin",
+            "Vodka",
+            "Whisky"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "What type of animal was Harambe, who was shot after a child fell into it&#039;s enclosure at the Cincinnati Zoo?",
+        "correct_answer": "Gorilla",
+        "incorrect_answers": [
+            "Tiger",
+            "Panda",
+            "Crocodile"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "How many colors are there in a rainbow?",
+        "correct_answer": "7",
+        "incorrect_answers": [
+            "8",
+            "9",
+            "10"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "What do the letters of the fast food chain KFC stand for?",
+        "correct_answer": "Kentucky Fried Chicken",
+        "incorrect_answers": [
+            "Kentucky Fresh Cheese",
+            "Kibbled Freaky Cow",
+            "Kiwi Food Cut"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "What company developed the vocaloid Hatsune Miku?",
+        "correct_answer": "Crypton Future Media",
+        "incorrect_answers": [
+            "Sega",
+            "Sony",
+            "Yamaha Corporation"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "When someone is inexperienced they are said to be what color?",
+        "correct_answer": "Green",
+        "incorrect_answers": [
+            "Red",
+            "Blue",
+            "Yellow"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "What is the closest planet to our solar system&#039;s sun?",
+        "correct_answer": "Mercury",
+        "incorrect_answers": [
+            "Mars",
+            "Jupiter",
+            "Earth"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "What is the name of NASA&rsquo;s most famous space telescope?",
+        "correct_answer": "Hubble Space Telescope",
+        "incorrect_answers": [
+            "Big Eye",
+            "Death Star",
+            "Millenium Falcon"
+        ]
+    }
+]
